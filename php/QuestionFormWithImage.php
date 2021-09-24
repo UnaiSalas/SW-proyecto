2916d73556277c5289468c6c380bf291143d4e28
UnaiSalas <61739021+UnaiSalas@users.noreply.github.com>
2021-09-24 14:32:21 +0200

    Lab1 no recoge la validación bien

--- php/QuestionFormWithImage.php
+++ php/QuestionFormWithImage.php
@@ -2,40 +2,40 @@
 <html>
 <head>
   <?php include '../html/Head.html'?>
-  <script type="text/javascript" src="../js/ValidateFieldsQuestionsJS.js"></script>
+  <script src="../js/ValidateFieldsQuestionsJS.js"></script>
   <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
 </head>
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-      <form id='fquestion' name='fquestion' action=’AddQuestion.php’ method="GET">
-        Email:
+      <form id='fquestion' name='fquestion' action='AddQuestion.php' method="GET">
+        <label for="email"> Email: </label>
         <input type="text" id="email" name="email" value=""/>
         <br />
-        Enunciado de la pregunta:
+        <label for="email"> Enunciado de la pregunta: </label>
         <input type="text" id="pregunta" name="pregunta" value=""/>
         <br />
-        Respuesta correcta:
+        <label for="email"> Respuesta correcta: </label>
         <input type="text" id="right_answ" name="right_answ" value=""/>
         <br />
-        Respuesta incorrecta:
+        <label for="email"> Respuesta incorrecta: </label>
         <input type="text" id="wrong_answ_1" name="wrong_answ_1" value=""/>
         <br />
-        Respuesta incorrecta:
+        <label for="email"> Respuesta incorrecta: </label>
         <input type="text" id="wrong_answ_2" name="wrong_answ_2" value=""/>
         <br />
-        Respuesta incorrecta:
+        <label for="email"> Respuesta incorrecta: </label>
         <input type="text" id="wrong_answ_3" name="wrong_answ_3" value=""/>
         <br />
-        Selecione la dificultad
+        <label for="email"> Selecione la dificultad: </label>
         <select name="dificultad" id="dificultad" >
           <option value="1">Baja</option>
           <option value="2">Media</option>
           <option value="3">Alta</option>
         </select>
         <br />
-        Tema (subject):
+        <label for="email"> Tema: </label>
         <input type="text" id="subject" name="subject" value=""/>
         <br />
         <input type="submit" value="Enviar" onClick="return validar_form();">
