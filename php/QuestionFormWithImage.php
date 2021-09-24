0ad80dd9ce60a34f07147e89fa85d906bd14a10f
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-09-24 17:53:41 +0000

    * a los formularios

--- php/QuestionFormWithImage.php
+++ php/QuestionFormWithImage.php
@@ -4,27 +4,28 @@
   <?php include '../html/Head.html'?>
 </head>
 <body>
+<script src="../js/jquery-3.4.1.min.js" type="text/javascript"></script>
   <script src="../js/ValidateFieldsQuestionJS.js"></script>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
       <form id='fquestion' name='fquestion' action='AddQuestion.php' method="GET" onsubmit="return validarform();">
-        <label for="email"> Email: </label>
+        <label for="email"> Email*: </label>
         <input type="text" id="email" name="email" value=""/>
         <br />
-        <label for="pregunta"> Enunciado de la pregunta: </label>
+        <label for="pregunta"> Enunciado de la pregunta*: </label>
         <input type="text" id="pregunta" name="pregunta" value=""/>
         <br />
-        <label for="right_answ"> Respuesta correcta: </label>
+        <label for="right_answ"> Respuesta correcta*: </label>
         <input type="text" id="right_answ" name="right_answ" value=""/>
         <br />
-        <label for="wrong_answ_1"> Respuesta incorrecta: </label>
+        <label for="wrong_answ_1"> Respuesta incorrecta*: </label>
         <input type="text" id="wrong_answ_1" name="wrong_answ_1" value=""/>
         <br />
-        <label for="wrong_answ_2"> Respuesta incorrecta: </label>
+        <label for="wrong_answ_2"> Respuesta incorrecta*: </label>
         <input type="text" id="wrong_answ_2" name="wrong_answ_2" value=""/>
         <br />
-        <label for="wrong_answ_3"> Respuesta incorrecta: </label>
+        <label for="wrong_answ_3"> Respuesta incorrecta*: </label>
         <input type="text" id="wrong_answ_3" name="wrong_answ_3" value=""/>
         <br />
         <label for="dificultad"> Selecione la dificultad: </label>
@@ -34,10 +35,10 @@
           <option value="3">Alta</option>
         </select>
         <br />
-        <label for="subject"> Tema: </label>
+        <label for="subject"> Tema*: </label>
         <input type="text" id="subject" name="subject" value=""/>
         <br />
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar" id="bEnviar">
       </form>
 
     </div>
