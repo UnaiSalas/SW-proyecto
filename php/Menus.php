81a5b58d582aba630b933a772b104ad969e74f3a
UnaiSalas <61739021+UnaiSalas@users.noreply.github.com>
2021-11-14 20:57:31 +0100

    menus

--- php/Menus.php
+++ php/Menus.php
@@ -11,13 +11,30 @@
 
 </header>
 <nav class='main' id='n1' role='navigation'>
-  <?php if(isset($_GET['correo'])){ ?>
-  <span><a href="Layout.php?correo=<?php echo $_GET["correo"]; ?>"> Inicio</a></span>
-  <span><a href="Credits.php?correo=<?php echo $_GET["correo"]; ?>">Creditos</a></span>
-  <span><a href="HandlingQuizesAjax.php?correo=<?php echo $_GET["correo"]; ?>"> Insertar Pregunta </a></span>
-  <span><a href="ShowQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas </a></span>
-  <span><a href="ShowXMLQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas XML</a></span>
-  <span><a href="ShowJsonQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas JSON</a></span>
+  <?php if(isset($_GET['correo'])){ 
+    $correo = $_GET['correo'];
+    $esProfesor = preg_match("/^([a-z]+\.)?[a-z]+@ehu\.(eus|es)$/", $correo);
+    if($esProfesor){?>
+      <span><a href="Layout.php?correo=<?php echo $_GET["correo"]; ?>"> Inicio</a></span>
+      <span><a href="Credits.php?correo=<?php echo $_GET["correo"]; ?>">Creditos</a></span>
+      <span><a href="HandlingQuizesAjax.php?correo=<?php echo $_GET["correo"]; ?>"> Insertar Pregunta </a></span>
+      <span><a href="ShowQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas </a></span>
+      <span><a href="ShowXMLQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas XML</a></span>
+      <span><a href="ShowJsonQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas JSON</a></span>
+      <span><a href="VipUsers.php?correo=<?php echo $_GET["correo"]; ?>"> Comprobar VIP</a></span>
+      <span><a href="ShowVips.php?correo=<?php echo $_GET["correo"]; ?>"> Listado de VIP</a></span>
+      <span><a href="AddVip.php?correo=<?php echo $_GET["correo"]; ?>"> Añadir VIP</a></span>
+      <span><a href="DeleteVip.php?correo=<?php echo $_GET["correo"]; ?>"> Borrar VIP</a></span>
+    <?php }else{ ?>
+      <span><a href="Layout.php?correo=<?php echo $_GET["correo"]; ?>"> Inicio</a></span>
+      <span><a href="Credits.php?correo=<?php echo $_GET["correo"]; ?>">Creditos</a></span>
+      <span><a href="HandlingQuizesAjax.php?correo=<?php echo $_GET["correo"]; ?>"> Insertar Pregunta </a></span>
+      <span><a href="ShowQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas </a></span>
+      <span><a href="ShowXMLQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas XML</a></span>
+      <span><a href="ShowJsonQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas JSON</a></span>
+    <?php } ?>
+    
+
 
   <?php }else { ?>
   <span><a href="Layout.php"> Inicio</a></span>
