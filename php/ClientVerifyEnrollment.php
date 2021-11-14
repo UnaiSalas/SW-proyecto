40bc47cc89f2fcb936884716487959d269f08b49
UnaiSalas <61739021+UnaiSalas@users.noreply.github.com>
2021-11-14 18:36:21 +0100

    parte 1 lab lista

--- php/ClientVerifyEnrollment.php
+++ php/ClientVerifyEnrollment.php
@@ -5,11 +5,17 @@
           $soapclient = new SoapClient('http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl');
           $method = 'comprobar';
 
-         
 
           //Llamamos a la funcion que habiamos implementado en el Web Service
           //y devolvemos el resultado
-          
-          echo '<h3> El correo ' . $correo . ' ' . $soapclient->comprobar($correo) . ' existe </h3>';
+          if($soapclient->comprobar($correo) == "SI"){
+            echo '<h3 style="color:Green;"> El correo ' . $correo . ' ' . $soapclient->comprobar($correo) . ' es válido </h3>';
+          }else{
+
+            echo '<h3 style="color:Red;"> El correo ' . $correo . ' ' . $soapclient->comprobar($correo) . ' es válido </h3>';
+          }
+
+
+        
           
         ?>
