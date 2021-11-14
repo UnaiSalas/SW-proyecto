e50effa52b6c0197ab50c76d6e605390b8a36709
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-11-14 18:40:14 +0000

    prueba correo vadillo

--- php/IsVip.php
+++ php/IsVip.php
@@ -31,13 +31,13 @@
       </style>
       
         <h1>Cliente REST para saber si el usuario es VIP</h1><br>
-        <form id="fisVip" name="fisVip" method="GET" action=<?php echo $_SERVER["PHP_SELF"];?>">
+        <form id="fisVip" name="fisVip" method="GET" action="<?php echo $_SERVER["PHP_SELF"];?>">
           <input type="text" id="id" name="id">
           <input type="button" id="esVIP" name="esVIP" value="Es VIP?"></button>
         </form>
         <?php
             $culr = curl_init();
-            $url = "https://sw.ikasten.io/~G24/LabWebServices/php/VipUsers.php?id=" . $_GET['id'];
+            $url = "https://sw.ikasten.io/~G24/LabWebServices/php/VipUsers.php?id=vadillo@ehu.eus";
             curl_setopt($culr, CURLOPT_URL, $url);
             curl_setopt($culr, CURLOPT_RETURNTRANSFER, 1);
             $str = curl_exec($culr);
